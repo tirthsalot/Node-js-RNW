3ae4e12cdd75fbc1ea3bea90df217744521d871b
Tirth Salot <salottirth09@gmail.com>
2026-09-24 14:54:51 +0530

    read and delete code added

--- 08_Theater_management_system/controller/Theater.controller.js
+++ 08_Theater_management_system/controller/Theater.controller.js
@@ -1,3 +1,4 @@
+import httpError from "../middleware/httpError.js";
 import httperror from "../middleware/httpError.js";
 
 import theatre from "../model/theater.model.js";
@@ -25,4 +26,65 @@ const add = async (req,res,next)=> {
         
     }
 };
-export default add;
+
+const getAllTheater = async (req,res,next)=>{
+    try {
+        const Theaters = await theatre.find({});
+
+        if(Theaters.length ===0) {
+            return next(new httpError("No Theater Data Found",404));
+        }
+
+        res.status(200).json({
+            success:true,
+            message:"theater data fetched successfully",
+            total: Theaters.length,
+            Theaters,
+        });
+        
+    } catch (error) {
+        return next(new httpError(error.message,500));
+        
+    }
+};
+
+const getTheaterById = async (req, res, next) => {
+  try {
+    const { id } = req.params;
+
+    const Theater = await Theater.findById(id);
+
+    if (!theatre) {
+      return next(new httpError("theater not found with this id", 404));
+    }
+
+    return res
+      .status(200)
+      .json({ success: true, message: "theater found", theatre });
+  } catch (error) {
+    return next(new httpError(error.message, 500));
+  }
+};
+
+
+const deleteTheaterById = async (req, res, next) => {
+  try {
+    const { id } = req.params;
+
+    const theater = await theatre.findByIdAndDelete(id);
+
+    
+
+    if (!theater) {
+      return next(new httpError("theater not found with this id", 404));
+    }
+
+    res
+      .status(200)
+      .json({ success: true, message: "theater deleted successfully" });
+  } catch (error) {
+    return next(new httpError(error.message, 500));
+  }
+};
+
+export default {add,getAllTheater,getTheaterById,deleteTheaterById};
--- 08_Theater_management_system/routes/theater.routes.js
+++ 08_Theater_management_system/routes/theater.routes.js
@@ -1,6 +1,11 @@
 import express from express;
-import theater from "../model/theater.model.js";
+import TheaterController from "../controller/Theater.controller.js";
 
 const router = express.Router();
 
-router.post("/add",theaterController.add);
+router.post("/add",TheaterController.add);
+router.get("/getAllTheater",TheaterController.getAllTheater);
+router.get("/:id",TheaterController.getTheaterbyId);
+router.delete("/:deleteTheater",TheaterController.deleteTheaterById);
+
+export default router
